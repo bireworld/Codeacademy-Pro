b6007da0fcac836ae02536e91d30f0c9875e3386
Chandara Ngoc <ngoc.chandara@gmail.com>
2019-04-20 07:53:40 -0400

    Added available_menus method
    The method reports what menu is available (not its contents).

--- Basta_Fazoolin.py
+++ Basta_Fazoolin.py
@@ -22,7 +22,14 @@ class Franchise ():
     
   def __repr__(self):
     return self.address
-    
+
+  def available_menus(self,time):
+    available_menu = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        available_menu.append(menu)
+    return available_menu
+  
 brunch = Menu("brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}, '11', '16')
     
 early_bird = Menu("early bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}, '15', '18')
@@ -36,8 +43,9 @@ all_menus = [brunch, early_bird, dinner, kids]
 flagship_store = Franchise("1232 West End Road", all_menus)
 new_installment = Franchise("12 East Mulberry Street", all_menus)
                             
-print(brunch)
-print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
-print(early_bird.calculate_bill(['salumeria plate','mushroom ravioli']))
-print(new_installment)
+#print(brunch)
+#print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+#print(early_bird.calculate_bill(['salumeria plate','mushroom ravioli']))
+#print(new_installment)
+#print(new_installment.available_menus(20))
   
